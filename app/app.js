13b39ca0690f27f1204ed7cff183ba0202e85065
medvmax <medvmax.com@mail.ru>
2016-12-24 18:09:21 +0300

    up

--- app/app.js
+++ app/app.js
@@ -8,29 +8,88 @@
 
     var configFunction = function ($mdThemingProvider, $stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('dashboard', {})
-            .state('login', {
-                url: '/login',
-                templateUrl: 'modules/login/view.htm'
-            });
+                .state('dashboard', {})
+                .state('login', {
+                    url: '/login',
+                    component: 'login',
+                    resolve: {
+//                        login: function() {
+//                                $mdDialog.show({
+//                                  controller: DialogController,
+//                                  templateUrl: 'modules/login/view.htm',
+//                                  clickOutsideToClose:true,
+//                                })
+//                                .then(function(answer) {
+//                                  $scope.status = 'You said the information was "' + answer + '".';
+//                                }, function() {
+//                                  $scope.status = 'You cancelled the dialog.';
+//                                });
+//                            }
+                        }
+                    });
+
         $urlRouterProvider.otherwise('/login');
-
         $mdThemingProvider.theme('default')
-            .primaryPalette('red', {
-                'default': '500',
-                'hue-1': '700',
-                'hue-2': '800'
-            })
-            .accentPalette('red', {
-                'default': 'A200'
-            });
+                .primaryPalette('green', {
+                    'default': '500',
+                    'hue-1': '700',
+                    'hue-2': '800'
+                })
+                .accentPalette('green', {
+                    'default': 'A200'
+                });
     };
 
-    var controllerFunction = function ($scope, $rootScope) {
-        $scope.main = 'work';
+    var controllerFunction = function ($scope, $mdDialog) {
+    $scope.showAdvanced = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'modules/login/view.htm',
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    }
+    function DialogController($scope, $mdDialog) {
+    $scope.user = {
+            login: "",
+            pass: ""
+        };
+    $scope.hide = function() {
+      $mdDialog.hide();
     };
 
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+  var serviceFunction = function(){
+    this.showDialog = function() {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'modules/login/view.htm',
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    }
     angular.module('app', dependencies)
-        .config(configFunction)
-        .controller('appController', controllerFunction);
+            .service('loginService', serviceFunction)
+            .config(configFunction)
+            .controller('appController', controllerFunction);
 })();
--- app/modules/login/login.js
+++ app/modules/login/login.js
@@ -1,3 +1,20 @@
-/**
- * Created by ilyushen on 21.12.2016.
- */
+'use strict';
+(function(){
+	var serviceFunction = function(){
+	this.showDialog = function() {
+    $mdDialog.show({
+      templateUrl: 'view.htm',
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+    }
+
+	angular.module('app', dependencies)
+            .service('loginService', serviceFunction);
+})();
