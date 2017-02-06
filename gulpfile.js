0e64360f0f150435233bafaea84f19c9e84d5a87
medvmax <medvmax.com@mail.ru>
2017-02-06 23:51:02 +0300

    gulp in progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,37 @@
 'use strict';
 
-var gulp = require('gulp')
-	sass = require('gulp-sass');
-
-// gulp.task('build', function  () {
-	
-// });
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	bower = require('gulp-bower');
+    // jshint = require('gulp-jshint');
 
 gulp.task('sass', function  () {
-	return gulp.src('app/*.scss')
+	return gulp.src('./app/**/*.scss')
 	.pipe(sass())
-	.pipe(gulp.dest('dist/css'))
+	.pipe(gulp.dest('./dist'))
 });
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./app/**/*.scss', ['sass']);
+});
+
+gulp.task('build', function () {
+    return gulp.src(['./app/**/*.js', './app/**'])
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task("jshint", function() {
+    gulp.src(devDir + "/js/**/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"));
+});
+
+var config = {
+    bowerDir: './dist/bower_components'
+};
+
+gulp.task('bower', function() {
+    return bower();
+});
+
+gulp.task('default', ['bower', 'sass', 'sass:watch']);
